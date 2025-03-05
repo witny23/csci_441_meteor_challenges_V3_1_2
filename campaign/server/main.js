a92d3e509c7afd8df069c430a15f106c40296761
Michael Whitney <whitneym@winthrop.edu>
2025-03-04 20:43:39 -0500

    06a - Basic MongoDB setup / communication challenge

--- campaign/server/main.js
+++ campaign/server/main.js
@@ -5,13 +5,18 @@ import {Meteor} from 'meteor/meteor';
 import {UP_Collection_Access} from './../imports/api/user_posts.js';
 // this gives us access to the UP_Collection_Access object so we can interact with the DB
 
-
+// promise: an object that represents the eventual completion or failure of an asynchronous operation and its resulting value.
+// async function: Marks a function as asynchronous, allowing the use of 'await' inside it which will make the function "pause" until the promise is resolved or rejected.
 Meteor.startup(async function(){
-
-
+  // challenge code should go below
 
   
+
+
+
+
+
+
+  // 'await' is used here to wait for the asynchronous 'find().fetch()' operation to complete before logging the collection data.
   console.log(await UP_Collection_Access.find().fetch());
-
-
 });
