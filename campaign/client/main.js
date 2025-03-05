6ac07264299d4013130902807ddcc89faecc4ac7
Michael Whitney <whitneym@winthrop.edu>
2025-03-05 12:09:13 -0500

    07a - 08b: db comm challenge

--- campaign/client/main.js
+++ campaign/client/main.js
@@ -18,35 +18,67 @@ const renderPosts = function (passed_posts) {
     return formattedPosts;
   };
   
+  const processFormDataFunction = function(event){
+    // the event (sometimes e) parameter is a default event handler object that is
+    // passed in by the browser when an event occurs
+    // this is an important argument becuase it allows us to access the topic name
+    // which we need in order to insert a new topic into the db
+    event.preventDefault() // stops the page from reloading
+    let newTopic = event.target.formInputNameAttribute.value;
+    // event.target grabs the target element - the form in this case which lets
+    // us grab any of its inputs by referencing it by name (.formInputNameAttribute)
+    // .value gets us the value
+    if (newTopic){
+      event.target.formInputNameAttribute.value = ''; // clear input box
+      UP_Collection_Access.insert({
+        topic: newTopic,
+      });
   
+    };
+  };
   Meteor.startup(async function () {
     
-    const posts =[{
-      _id: '01',
-      topic: 'cats',
-      votes: 5,
-    }, {
-      _id: '02',
-      topic: 'dogs',
-      votes: 2,
-    }, {
-      _id: '03',
-      topic: 'birds',
-      votes: 11,
-    }
-];
-let title = '441 reddit';
-let jsx = (
-  <div>
-    <h1>{title}</h1>
+  // Tracker tracks queries and reruns code when queries change
+  Tracker.autorun(function(){
+    const allPostsInDB = UP_Collection_Access.find().fetch();
+    let title = 'DB Practice';
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        <h2>Instructions</h2>
+        <p>The purpose of this challenge is to practice rendering db info into the browser. To that end,
+            accomplish the following:
+        </p>
+        <ul>
+          <li>In api/user_posts.js: create a collection called random_thoughts. Export as needed.</li>
+          <li>In client/main.js:
+            <ul>
+              <li>import the db object</li>
+              <li>Create a form to obtain random thoughts from the user</li>
+              <li>Create a function similar to processFormDataFunction to enter random thoughts into db</li>
+              <li>Create a function similar to renderPosts to render the random thoughts as imported</li>
+              <li>Create a table with two columns. Use one column to render current topics and the other to render random thoughts</li>
+            </ul>
+          </li>
+          <li>Bonus:
+              <ul>
+                <li>Create a means to change the style of anything (e.g., background-color) based on user input</li>
+              </ul>  
+          </li>
+        </ul>
+        <form onSubmit={processFormDataFunction}>
+          <input type='text' name='formInputNameAttribute' placeholder='Topic Name'/>
+          <button>Add Topic</button>
+        </form>
+        {renderPosts(allPostsInDB)}
+      </div>
+    );
 
-    {/* renderPosts('hi') */}
-    {renderPosts(posts)}
-  </div>
-);
+    ReactDOM.render(jsx, document.getElementById('content'));
 
-ReactDOM.render(jsx, document.getElementById('content'));
+  });
+
+
+
 
 });
-  
-  
--- campaign/server/main.js
+++ campaign/server/main.js
@@ -7,15 +7,11 @@ import {UP_Collection_Access} from './../imports/api/user_posts.js';
 
 
 Meteor.startup(async function(){
-    UP_Collection_Access.insertAsync({
-      topic: 'dog',
-      votes: 9,
-    });
 
-    UP_Collection_Access.insertAsync({
-      topic: 'bird',
-      votes: 93,
-    });
+
+
+
+
 
   console.log(await UP_Collection_Access.find().fetch());
   // await ensures each insertAsync() completes before moving to the next line.
@@ -23,4 +19,28 @@ Meteor.startup(async function(){
   // .fetch() is a pointer to some documents in the DB
   // to get an array of the documents you use .fetch()
 
+
+
+
+
+
+
+
+  // The following allows the client to insert, remove, and update data from the collection
+  // Allowing all inserts from the client is a Security risk
+  // Anyone can open the browser console and run:
+  // UP_Collection_Access.insert({ topic: "Hacked!", votes: 9999 });
+
+  UP_Collection_Access.allow({
+    insert(userId, doc) {
+      return true; // Anyone can insert
+    },
+    remove(userId, doc) {
+      return true; // Anyone can remove
+    },
+    update(userId, doc, fieldNames, modifier) {
+      return true; // Anyone can update
+    },
+  });
 });
+
