098da2a1c8c810d5377d0df313bd882c3f49ea88
Michael Whitney <whitneym@winthrop.edu>
2025-03-30 22:18:42 -0400

    vids 23a - 25a: scss Challenge[s]

--- campaign/imports/ui/Instructions.js
+++ campaign/imports/ui/Instructions.js
@@ -7,8 +7,8 @@ export default class Instructions extends React.Component{
       <h1>Instructions</h1>
       <p>The purpose of these challenges is to practice implementing scss techniques in our application
       </p>
-      <h2>Part 1: Set up your style to use rem (root element's font size)units (vid 24a)</h2>
-      <li>client/main.scss:
+      <h2>Part 1: Set up your style to use rem (root element's font size) units (vid 24a)</h2>
+      <li>imports/client/styles/main.scss:
           <ul>
             <li>Perform a reset of the margin and padding (set to 0) for everything using the universal selector (*).</li>
             <li>Set the font size of your html to 62.5% which is setting everying to be 10px</li>
@@ -18,14 +18,14 @@ export default class Instructions extends React.Component{
           </ul>
       </li>
       <h2>Part 2: Stylize the TitleBar (vid 24a)</h2>
-      <li>client/main.scss:
+      <li>imports/client/styles/main.scss:
           <ul>
-            <li>Create a class for the title bar. Then:
+            <li>Create a class selector for the title bar. Then:
               <ul>
                 <li>provide a font and background color different than that set on the body</li>
               </ul>
             </li>
-            <li>Create another class to be used to center the title bar. Then:
+            <li>Create another class selector to be used to center the title bar. Then:
               <ul>
                 <li>center that class by using the margin / auto technique</li>
                 <li>limit the max width of contents in your class using rem (pick any size that is small enough to notice)</li>
@@ -38,36 +38,25 @@ export default class Instructions extends React.Component{
       </li>
       <li>imports/ui/TitleBar.js:
           <ul>
-            <li>Apply your previously developed class for the titleBar color and background on the TitleBar using className</li>
+            <li>Apply your previously developed class selector for the titleBar color and background on the TitleBar using className</li>
             <li>Use your centering class to center the titleBar</li>
           </ul>
       </li>
       
       <h2>Part 3: scss imports (vid 25a)</h2>
-      <li>Create a new imports/client/styles/_main.scss partial (note there are two directories)
-          <ul>
-            <li>cut everything out of the client/main.scss and paste it into this partial</li>
-          </ul>
-      </li>
       
-      <li>client/main.scss:
+      <li>Create a new components directory in imports/client/styles</li>
+      <li>Create a new imports/client/styles/<strong>components</strong>/_titleBar.scss
           <ul>
-            <li>import the imports/client/styles/_main.scss</li>
-            
+            <li>Move the class selector for the titleBar's color and background located in main.scss into this partial</li>
           </ul>
       </li>
-      <li>Test to make sure your style loads</li>
-      <li>Create a new imports/client/styles/components/_titleBar.scss
+      <li>Create a new imports/client/styles/<strong>components</strong>/_wrapper.scss
           <ul>
-            <li>Move the one class selector for the titleBar color and background located in _main.scss into this partial</li>
+            <li>Move the class selector for centering the titleBar located in main.scss into this partial</li>
           </ul>
       </li>
-      <li>Create a new imports/client/styles/components/_wrapper.scss
-          <ul>
-            <li>Move the one class selectors for centering the titleBar located in _main.scss into this partial</li>
-          </ul>
-      </li>
-      <li>imports/client/styles/_main.scss
+      <li>imports/client/styles/main.scss
           <ul>
             <li>import the _titleBar.scss</li>
             <li>import the _wrapper.scss</li>
